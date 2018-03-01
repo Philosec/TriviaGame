0db868eff77331beb137532cc0ac4ea99670122e
Shane Padgett <spadgett00@gmail.com>
2018-02-28 20:20:57 -0600

    Add start game button and update question card layout

---
+++ assets/scripts/application.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+  $('.answer-1').parent().on('click', event => {
+    $('.check').removeClass('d-none');
+    $('.question-text').addClass('mt-2 mb-0');
+    $('.answer-1').parent().removeClass('btn-info');
+    $('.answer-1').parent().addClass('btn-danger');
+    $('.answer-2').parent().removeClass('btn-info');
+    $('.answer-2').parent().addClass('btn-success');
+  });
+
+  $('.answer-2').parent().on('click', event => {
+    $('.check').addClass('d-none');
+    $('.question-text').removeClass('mt-2 mb-0');
+    $('.answer-1').parent().removeClass('btn-danger');
+    $('.answer-1').parent().addClass('btn-info');
+    $('.answer-2').parent().removeClass('btn-success');
+    $('.answer-2').parent().addClass('btn-info');
+  });
+});
