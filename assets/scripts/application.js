820834fc45f6c27eb42676e828c4d626461d1b97
Shane Padgett <spadgett00@gmail.com>
2018-03-02 17:30:18 -0600

    Added sound button

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -1,8 +1,25 @@
 $(document).ready(() => {
   'use strict'
 
-
   var game = new Game()
 
   game.init()
+
+  var audio = document.createElement('audio')
+  audio.src = 'assets/audio/mostAstounding.mp3'
+  audio.play()
+
+  $('.audio-btn-on').on('click', () => {
+    audio.muted = true
+    audio.pause()
+    $('.audio-btn-off').removeClass('d-none')
+    $('.audio-btn-on').addClass('d-none')
+  })
+
+  $('.audio-btn-off').on('click', () => {
+    audio.muted = false
+    audio.play()
+    $('.audio-btn-on').removeClass('d-none')
+    $('.audio-btn-off').addClass('d-none')
+  })
 })
