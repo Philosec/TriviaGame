84a5f25d5164b921e74073ea660a6eb481b7f263
Shane Padgett <spadgett00@gmail.com>
2018-03-02 19:32:30 -0600

    Update questions

--- assets/scripts/question_bank.js
+++ assets/scripts/question_bank.js
@@ -22,7 +22,7 @@ QuestionBank.prototype.initQuestions = function () {
   this.questionArr.push(new Question('What type of galaxy is the most common', 'Elliptical', ['Spiral', 'Barred Spiral', 'Irregular'], 'assets/images/galaxy.png'))
   this.questionArr.push(new Question('How old is the universe in light years', '13.8 Billion', ['5.2 Billion', '2.4 Billion', '1.1 Billion'], 'assets/images/universe.png'))
   this.questionArr.push(new Question('What is the mass of the Earth\'s sun', '2 Quintillion kg', ['1 Quintillion kg', '1 Billion kg', '2 Quadrillion kg'], 'assets/images/sun.png'))
-  this.questionArr.push(new Question('What\'s the distance from earth to moon', '238,900 mi', ['420,000 mi', '383,000 mi', '180,000 mi'], 'assets/images/moon.png'))
+  this.questionArr.push(new Question('Distance from the earth to moon', '238,900 mi', ['420,000 mi', '383,000 mi', '180,000 mi'], 'assets/images/moon.png'))
   this.questionArr.push(new Question('Number of planets in our solar system', '8', ['7', '9', '10'], 'assets/images/solar-system.png'))
   this.questionArr.push(new Question('Planet with no solid surface', 'Jupiter', ['Mars', 'Earth', 'Mercury'], 'assets/images/2-planets.png'))
   this.questionArr.push(new Question('What color is the sunset on Mars', 'Blue', ['Red', 'Yellow', 'Green'], 'assets/images/sunset.png'))
